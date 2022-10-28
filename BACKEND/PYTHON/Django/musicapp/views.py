1853992392b8a2f4edd52ef7cbc240a60f3465e5
Alexditah <99592915+Alexditah@users.noreply.github.com>
2022-10-28 19:01:56 +0100

    Update views.py

--- BACKEND/PYTHON/Django/musicapp/views.py
+++ BACKEND/PYTHON/Django/musicapp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-# Create your views here.
 
 def index(request):
     return HttpResponse("<h1>Hello, This is my first web app.</h1>")
