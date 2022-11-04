56ef081e4d7ee2fe3c04849451a03df9a6667d76
Alexditah <99592915+Alexditah@users.noreply.github.com>
2022-11-04 18:21:24 +0100

    Update views.py

--- BACKEND/PYTHON/Django/musicapp/views.py
+++ BACKEND/PYTHON/Django/musicapp/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Song, Artiste 
+from .serializers import SongSerializer, ArtisteSerializer
 
-def index(request):
-    return HttpResponse("<h1>Hello, This is my first web app.</h1>")
+class SongList(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer   
+
+class ArtisteList(generics.ListCreateAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer
+
+class ArtisteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer  
